259c3f865be0c9a72d4f14d3bf030eec615a1a0e
NFA <novran@zerobyte.id>
2022-12-08 13:57:11 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,5 +2,5 @@
 
 OWNDIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 
-python3 ${OWNDIR}/doRecon.py
-python3 ${OWNDIR}/webscanfromelastic.py
+python3 ${OWNDIR}/autorecon.py
+python3 ${OWNDIR}/webscan.py
